205335322f8badf43d6738bf3af800c44bd949a4
thehundude <tamas.reisz@outlook.com>
2017-08-20 15:37:07 +0200

    4. feladat: lapozás megoldva (itt most épp 5 / lap, de könnyen átírható)

--- client/app/modules/movie/allorders.controller.js
+++ client/app/modules/movie/allorders.controller.js
@@ -8,4 +8,19 @@ moviesApp.controller('allOrdersController',
                 $scope.orders = response.data;
             })
         }
-    });
+
+        // lapozós funkció: https://stackoverflow.com/questions/11581209/pagination-on-a-list-using-ng-repeat
+        $scope.currentpage = 0;
+        $scope.pageSize = 5;
+        $scope.numberOfPages = function() {
+            return Math.ceil($scope.orders.length/$scope.pageSize)
+        }
+    },
+
+    moviesApp.filter('startFrom', function(){
+        return function(input, start) {
+            start = +start;
+            return input.slice(start);
+        }
+    })
+);
